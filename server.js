0d20de7a95809d13ba8bb2ae2c292e140e2e6456
Joaquim707 <158746033+Joaquim707@users.noreply.github.com>
2025-09-04 21:47:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.use("/api/reports", reportRoutes);
 // Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
